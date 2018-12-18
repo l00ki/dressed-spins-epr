e29b2dc5390703d501dd9daf39c30657bff92eb6
Lukas Schreder <schreluk@ethz.ch>
2018-12-18 07:25:00 +0100

    update numerical simulation

--- sim_dsr1d.m
+++ sim_dsr1d.m
@@ -13,7 +13,7 @@ Ia=cos(eta)^2;
 
 % pulsing
 nu1 = 6.8;  % channel 1 power [MHz]
-nu2 =  0.5000; % channel 2 power [MHz]
+nu2 =  0.2000; % channel 2 power [MHz]
 nuRF0 = 0.000; % sweep from this rf frequency [MHz]
 nuRF = 10.000; % sweep to this rf frequency [MHz]
 tp  = 0.25/nu1; % prep pulse length [us]
@@ -21,8 +21,8 @@ tau =  0.6;  % interpulse evolution time [us]
 T   =  22 * tp; % HTA pulse + rf irradiation length [us]
 
 % sampling
-nRF = 21; % rf frequencies probed
-nOffset = 100; % spin packages
+nRF = 51; % rf frequencies probed
+nOffset = 50; % spin packages
 
 %% setup
 Sx = sop(sys, 'xe');
@@ -40,39 +40,39 @@ signal = zeros(1, nRF);
 dT=1e-3;
 nT = round(T / dT);
 
-tic
-parfor iRF = 1 : nRF
-    disp(iRF)
-    for iOffset = 1 : nOffset
-        H0 = nuOffset(iOffset) * Sz + A * Sz * Iz + B * Sz * Ix + nuI * Iz;
-        s = Sz;
-        
-        H1 = H0 + nu1 * Sx;
-        H2 = H0 - nu1 * Sx;
-        U1 = expm(-2i * pi * tp * H1);
-        U2 = expm(-2i * pi * tp * H2);
-        s = (U1 * s * U1' - U2 * s * U2') / 2;
-        
-        H = H0;
-        U = expm(-2i * pi * tau * H);
-        s = U * s * U';
-        
-        t = 0;
-        for iT = 1 : nT
-            H = H0 + nu1 * Sy + 2 * nu2 * cos(2 * pi * nuRF(iRF) * t) * (Sz);
-            U = expm(-2i * pi * H * dT);
-            s = U * s * U';
-            t = t + dT;
-        end
-        
-        H = H0;
-        U = expm(-2i * pi * tau * H);
-        s = U * s * U';
-        
-        signal(iRF) = signal(iRF) + weights(iOffset) * trace(M * s);
-   end
-end
-toc
+% tic
+% parfor iRF = 1 : nRF
+%     disp(iRF)
+%     for iOffset = 1 : nOffset
+%         H0 = nuOffset(iOffset) * Sz + A * Sz * Iz + B * Sz * Ix + nuI * Iz;
+%         s = Sz;
+%         
+%         H1 = H0 + nu1 * Sx;
+%         H2 = H0 - nu1 * Sx;
+%         U1 = expm(-2i * pi * tp * H1);
+%         U2 = expm(-2i * pi * tp * H2);
+%         s = (U1 * s * U1' - U2 * s * U2') / 2;
+%         
+%         H = H0;
+%         U = expm(-2i * pi * tau * H);
+%         s = U * s * U';
+%         
+%         t = 0;
+%         for iT = 1 : nT
+%             H = H0 + nu1 * Sy + 2 * nu2 * cos(2 * pi * nuRF(iRF) * t) * (Sz);
+%             U = expm(-2i * pi * H * dT);
+%             s = U * s * U';
+%             t = t + dT;
+%         end
+%         
+%         H = H0;
+%         U = expm(-2i * pi * tau * H);
+%         s = U * s * U';
+%         
+%         signal(iRF) = signal(iRF) + weights(iOffset) * trace(M * s);
+%    end
+% end
+% toc
 
 %% plot
 figure(2)
@@ -119,8 +119,10 @@ parfor iRF = 1 : nRF
 end
 toc
 
+%% plot
 signal = real(signal / max(signal));
-plot(nuRF, signal, 'r')
-legend('HF', 'no HF')
+signalsmooth = datasmooth(signal, 10);
+plot(nuRF, signalsmooth, 'r')
+ylim([0.5 1])
 
 print('~/git/uni-writing/res/dressed-spins-epr/sim_dsr1d', '-dpng')
